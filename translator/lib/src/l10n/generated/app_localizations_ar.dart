59d40db71b9adb0e31c05cdf7fa328b6af009736
Samir Hassan <samir@innosoft.me>
2024-02-23 08:59:42 +0200

    GENERATE: app localizations files

---
+++ translator/lib/src/l10n/generated/app_localizations_ar.dart
@@ -0,0 +1,9 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([super.locale = 'ar']);
+
+  @override
+  String get appName => 'موظفو الحضانات';
+}
